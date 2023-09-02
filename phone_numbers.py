20f8c50076f8701852dced5931176b6cb68b5f65
ArtemB118 <1evkakii1@gmail.com>
2023-09-02 19:57:35 +0600

    DONE

--- phone_numbers.py
+++ phone_numbers.py
@@ -20,4 +20,28 @@ for el in contacts_list:
 for el in range(len(name_list)):
     contacts_list[el][:3] = name_list[el][:3]
 
-print(name_list)
+pattern = re.compile(r"(\+?(\d{1,4}?)[-.\s]?\(?(\d{3}?)\)?[-.\s]?(\d{1,4})[-.\s]?(\d{2})[-.\s]?(\d{2})[-.\s]?)")
+pattern2 = re.compile(r"(\+?(\d{1,4}?)[-.\s]?\(?(\d{3}?)\)?[-.\s]?(\d{1,4})[-.\s]?(\d{2})[-.\s]?(\d{2})[-.\s]?\(?\(?доб\.\s?(\d{4})\)?)")
+subs_pattern = r"+7(\3)\4-\5-\6"
+subs_pattern2 = r"+7(\3)\4-\5-\6 доб.\7"
+for el in contacts_list:
+    el[5] = re.sub(pattern, subs_pattern, el[5])
+    el[5] = re.sub(pattern2, subs_pattern2, el[5])    
+unique_dict = {}
+
+for item in contacts_list:
+    key = tuple(item[:2])
+
+    if key in unique_dict:
+        unique_dict[key].extend(item[2:])
+    else:
+        unique_dict[key] = item
+
+almost_done = list(unique_dict.values())
+
+for sublist in almost_done:
+    sublist[:] = [item for item in sublist if item != '']
+
+with open("phonebook.csv", "w", encoding="utf-8") as f:
+  datawriter = csv.writer(f, delimiter=',')
+  datawriter.writerows(almost_done)
